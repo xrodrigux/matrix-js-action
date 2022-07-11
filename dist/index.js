f1b5712e58cd38a3052f4cdce07b6f9c0c6f8e40
Xavier Rodriguez <xerodriguez@gmail.com>
2022-07-11 13:51:19 -0300

    logging info

--- dist/index.js
+++ dist/index.js
@@ -9010,6 +9010,8 @@ const main = async () => {
         const readmeIDFiles = core.getInput('readme-id-file', { required: true });
         const filesToProcess = core.getInput('files-to-process', { required: true });
         const octokit = new github.getOctokit(core.getInput('github-token', { required: true }));
+        console.log('READMEIDFILES = ', readmeIDFiles);
+        console.log('FILESTOPROCESS = ', filesToProcess);
         // name is data because it's taken from response body 
         const { data } = await octokit.rest.repos.getContent({
             mediaType: { format: "raw" },
@@ -9017,11 +9019,13 @@ const main = async () => {
             repo: github.context.repo.repo,
             path: `${readmeIDFiles}`
         });
+        console.log('DATA ', data);
         let readmeFiles = JSON.parse(data);
         let searchArray = filesToProcess.split(",");
         let toUpdate = readmeFiles.filter(f => searchArray.includes(f.file));
         console.log(toUpdate);
         core.setOutput('update-matrix', JSON.stringify(toUpdate));
+        core.setOutput('has-updates-to-process', (toUpdate.length > 0 ? 1 : 0));
     } catch (error) {
         console.log(error);
         core.setFailed(error.message);
