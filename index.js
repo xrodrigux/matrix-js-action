37c63e444e2d3f7dc1034455792583d63ae5b45c
Xavier Rodriguez <xerodriguez@gmail.com>
2022-07-11 10:25:19 -0300

    test 19 - change source of files changed

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const github = require('@actions/github');
 const main = async () => {
     try {
         const readmeIDFiles = core.getInput('readme-id-file', { required: true });
-        const filesToProcess = core.getInput('files-to-process', { required: true });
+        const filterExpression = core.getInput('files-filer-expression', { required: true });
+        const branchName = core.getInput('branch-name', { required: true });
+        console.log('BRANCHNAME', branchName);
+        console.log('FILTER_EXPRESSION', filterExpression);
         const octokit = new github.getOctokit(core.getInput('github-token', { required: true }));
         const { data } = await octokit.rest.repos.getContent({
             mediaType: { format: "raw" },
             owner: github.context.repo.owner,
             repo: github.context.repo.repo,
             path: `${readmeIDFiles}`,
+            ref: branchName
         });
         let readmeFiles = JSON.parse(data);
         let searchArray = filesToProcess.split(",");
